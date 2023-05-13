181b26da8d705190ae28a032df58d0aaa287ab7f
oritzhaki <117988679+oritzhaki@users.noreply.github.com>
2023-05-13 11:43:12 +0300

    Update ex22.c

--- ex22.c
+++ ex22.c
@@ -479,6 +479,9 @@ int main(int argc, char *argv[]) {
                     closedir(main_dir);
                     closedir(user_dir);
                     if (write(1, "Error in: stat\n", 15) == -1){
+                        write(1, "user entry in stat: ", 20);
+                        write(1, user_entry->d_name, strlen(user_entry->d_name));
+                        write(1, "\n", 1);
                         return -1;
                     }
                 }
@@ -489,9 +492,9 @@ int main(int argc, char *argv[]) {
             }
         }
         
-        write(1, "user entry out of loop: ", 24);
-        write(1, user_entry->d_name, strlen(user_entry->d_name));
-        write(1, "\n", 1);
+//         write(1, "user entry out of loop: ", 24);
+//         write(1, user_entry->d_name, strlen(user_entry->d_name));
+//         write(1, "\n", 1);
         
         if (!user_entry){//no c file, move on to other user  -c_flag 
             char full_result[150];
